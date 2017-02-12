562f5414284b408648286e474e70fe361ad7ca50
Marco Mravic <mmravic@iq218.(none)>
2017-02-11 20:50:44 -0800

    just after docking runtime

--- rosi/docking/qsub_aV_B3_docking.sh
+++ rosi/docking/qsub_aV_B3_docking.sh
@@ -1,14 +1,14 @@
 #!/bin/bash                         #-- what is the language of this shell
 #                                  #-- Any line that starts with #$ is an instruction to SGE
 #$ -S /bin/bash                     #-- the shell for the job
-#$ -o /netapp/home/mmravic/CHAMP/logs                      #-- output directory (fill in)
+#$ -o /netapp/home/mmravic/CHAMP/modeling_aVb3/rosi/docking #-- output directory (fill in)
 #$ -cwd                            #-- tell the job that it should start in your working directory
 #$ -r y                            #-- tell the system that if a job crashes, it should be restarted
 #$ -j y                            #-- tell the system that the STDERR and STDOUT should be joined
 #$ -l mem_free=1G                  #-- submits on nodes with enough free memory (required)
 #$ -l arch=linux-x64               #-- SGE resources (CPU type)
 #$ -l netapp=1G,scratch=1G         #-- SGE resources (home and scratch disks)
-#$ -l h_rt=24:00:00                #-- runtime limit (see above; this requests 24 hours)
+#$ -l h_rt=60:00:00                #-- runtime limit (see above; this requests 24 hours)
 #$ -t 1-20                        #-- remove first '#' to specify the number of
                                    #-- tasks if desired (see Tips section)
 
@@ -16,13 +16,13 @@
 inx=1
 
 #Determine which job/index to stop on
-#taskID=$SGE_TASK_ID
-taskID=18
+taskID=$SGE_TASK_ID
+#taskID=10
 
 ## Loop through a list, incrementing $inx each time. 
 # Here, list is an array of directory pathes: ms1_trial_1, ms1_trial_2, ...etc
 # The looping variable is the path to each directory
-for pdb in /home/xray/CHAMP/modeling_aVb3/rosi/docking/prepack*.pdb
+for pdb in /netapp/home/mmravic/CHAMP/modeling_aVb3/rosi/docking/prepack*.pdb
 	
 	do 
 	
@@ -32,7 +32,8 @@ for pdb in /home/xray/CHAMP/modeling_aVb3/rosi/docking/prepack*.pdb
 				echo $pdb
 				## Perform job script... here a python script that calles rosetta... 
 				# but this line can more simply be the command line calling rosetta itself
-				~/rosetta/source/bin/mp_dock.linuxgccrelease -in:file:s $pdb -score:weights mpframework_docking_fa_2015.wts -mp:setup:spanfiles aV_B3_apart.span -mp:scoring:hbond -packing:pack_missing_sidechains 0 -docking:partners A_B -nstruct 1 -out:overwrite -out:prefix outputs/ -ignore_zero_occupancy false
+				/netapp/home/mmravic/bin/Rosetta/source/bin/mp_dock.linuxgccrelease -in:file:s $pdb -score:weights mpframework_docking_fa_2015.wts -mp:setup:spanfiles aV_B3_apart.span -mp:scoring:hbond -packing:pack_missing_sidechains 0 -docking:partners A_B -nstruct 3000 -out:overwrite -out:prefix outputs/ -ignore_zero_occupancy false > std.txt
+                                rm std.txt
 				break 
 
 			fi
